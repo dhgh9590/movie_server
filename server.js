751d8bd3a438d32865a89210cd0af5d90cd9c409
hoseol OH <dhgh9590@naver.com>
2022-12-01 17:07:27 +0900

    배포 수정

--- server.js
+++ server.js
@@ -59,3 +59,14 @@ app.post("/search", function (req, res) {
       res.json({ searchItem: 결과 });
     });
 });
+
+//메인 페이지
+app.use(express.static(path.join(__dirname, "movieapp/out")));
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, "/movieapp/out/index.html"));
+});
+
+app.get("*", function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, "/movieapp/out/index.html"));
+});
